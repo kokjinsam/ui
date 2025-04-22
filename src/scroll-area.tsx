d0fe1c5695e7497b2a7ada429e338dfa3efafcd3
Kok-Jin Sam <9153498+kokjinsam@users.noreply.github.com>
2025-04-22 19:36:47 +0800

    Update

--- src/scroll-area.tsx
+++ src/scroll-area.tsx
@@ -3,15 +3,12 @@ import * as React from "react"
 import { cn } from "./utils"
 
 const ScrollArea = ({
-  ref,
-  className,
   children,
   ...props
 }: React.ComponentProps<typeof ScrollAreaPrimitive.Root>) => (
   <ScrollAreaPrimitive.Root
-    ref={ref}
-    className={cn("relative overflow-hidden", className)}
     {...props}
+    className={cn("relative overflow-hidden", props.className)}
   >
     <ScrollAreaPrimitive.Viewport className="h-full w-full rounded-[inherit]">
       {children}
@@ -22,23 +19,20 @@ const ScrollArea = ({
 )
 
 const ScrollBar = ({
-  ref,
-  className,
   orientation = "vertical",
   ...props
 }: React.ComponentProps<typeof ScrollAreaPrimitive.ScrollAreaScrollbar>) => (
   <ScrollAreaPrimitive.ScrollAreaScrollbar
-    ref={ref}
     orientation={orientation}
+    {...props}
     className={cn(
       "flex touch-none transition-colors select-none",
       orientation === "vertical" &&
         "h-full w-2.5 border-l border-l-transparent p-[1px]",
       orientation === "horizontal" &&
         "h-2.5 flex-col border-t border-t-transparent p-[1px]",
-      className
+      props.className
     )}
-    {...props}
   >
     <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-1 rounded-full bg-[var(--ui3)]" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
