5aeeec5a54094a480706309ccd3228cc7225358b
Kok-Jin Sam <9153498+kokjinsam@users.noreply.github.com>
2025-04-23 20:13:41 +0800

    Update

--- src/menu.tsx
+++ src/menu.tsx
@@ -5,8 +5,7 @@ import type {
   MenuProps as MenuPrimitiveProps,
   MenuSectionProps as MenuSectionPrimitiveProps,
   MenuTriggerProps as MenuTriggerPrimitiveProps,
-  PopoverProps,
-  SubmenuTriggerProps as SubmenuTriggerPrimitiveProps
+  PopoverProps
 } from "react-aria-components"
 import {
   Button,
@@ -19,6 +18,7 @@ import {
   SubmenuTrigger as SubmenuTriggerPrimitive,
   composeRenderProps
 } from "react-aria-components"
+import type { VariantProps } from "tailwind-variants"
 import {
   DropdownItemDetails,
   DropdownKeyboard,
@@ -28,32 +28,13 @@ import {
   dropdownSectionStyles
 } from "./dropdown"
 import { PopoverContent } from "./popover"
-import type { VariantProps } from "./utils"
 import { cn } from "./utils"
 
-type MenuContextProps = {
-  respectScreen: boolean
-}
+type MenuProps = MenuTriggerPrimitiveProps
 
-const MenuContext = React.createContext<MenuContextProps>({
-  respectScreen: true
-})
+const Menu = (props: MenuProps) => <MenuTriggerPrimitive {...props} />
 
-type MenuProps = MenuTriggerPrimitiveProps & {
-  respectScreen?: boolean
-}
-
-const Menu = ({ respectScreen = true, ...props }: MenuProps) => (
-  <MenuContext value={{ respectScreen }}>
-    <MenuTriggerPrimitive {...props}>{props.children}</MenuTriggerPrimitive>
-  </MenuContext>
-)
-
-type MenuSubMenuProps = SubmenuTriggerPrimitiveProps & {
-  delay?: number
-}
-
-const MenuSubMenu = ({ delay = 0, ...props }: MenuSubMenuProps) => (
+const MenuSubMenu = ({ delay = 0, ...props }) => (
   <SubmenuTriggerPrimitive {...props} delay={delay}>
     {props.children}
   </SubmenuTriggerPrimitive>
@@ -65,11 +46,9 @@ const MenuTrigger = (props: MenuTriggerProps) => (
   <Button
     data-slot="menu-trigger"
     {...props}
-    className={composeRenderProps(props.className, (className) =>
-      cn(
-        "focus-visible:ring-primary relative inline text-left outline-hidden focus-visible:ring-1",
-        className
-      )
+    className={cn(
+      props.className,
+      "focus-visible:ring-primary relative inline text-left outline-hidden focus-visible:ring-1"
     )}
   >
     {(values) => (
@@ -94,47 +73,42 @@ type MenuContentProps<T> = Pick<
   | "shouldFlip"
 > &
   MenuPrimitiveProps<T> & {
-    className?: string
-    popoverClassName?: string
+    classNames?: {
+      popover?: string
+    }
     showArrow?: boolean
-    respectScreen?: boolean
   }
 
 const MenuContent = <T extends object>({
+  classNames,
   showArrow = false,
   ...props
-}: MenuContentProps<T>) => {
-  const { respectScreen } = React.use(MenuContext)
-
-  return (
-    <PopoverContent
-      isOpen={props.isOpen}
-      onOpenChange={props.onOpenChange}
-      shouldFlip={props.shouldFlip}
-      respectScreen={respectScreen}
-      showArrow={showArrow}
-      offset={props.offset}
-      placement={props.placement}
-      crossOffset={props.crossOffset}
-      triggerRef={props.triggerRef}
-      arrowBoundaryOffset={props.arrowBoundaryOffset}
-      className={composeRenderProps(props.popoverClassName, (className) =>
-        cn("z-50 p-0 shadow-xs outline-hidden sm:min-w-40", className)
+}: MenuContentProps<T>) => (
+  <PopoverContent
+    isOpen={props.isOpen}
+    onOpenChange={props.onOpenChange}
+    shouldFlip={props.shouldFlip}
+    showArrow={showArrow}
+    offset={props.offset}
+    placement={props.placement}
+    crossOffset={props.crossOffset}
+    triggerRef={props.triggerRef}
+    arrowBoundaryOffset={props.arrowBoundaryOffset}
+    className={cn(
+      "z-50 p-0 shadow-xs outline-hidden sm:min-w-40",
+      classNames?.popover
+    )}
+  >
+    <MenuPrimitive
+      data-slot="menu-content"
+      className={cn(
+        "grid max-h-[calc(var(--visual-viewport-height)-10rem)] grid-cols-[auto_1fr] overflow-auto rounded-xl p-1 outline-hidden [clip-path:inset(0_0_0_0_round_calc(var(--radius-lg)-2px))] sm:max-h-[inherit] *:[[role='group']+[role=group]]:mt-4 *:[[role='group']+[role=separator]]:mt-1",
+        props.className
       )}
-    >
-      <MenuPrimitive
-        data-slot="menu-content"
-        {...props}
-        className={composeRenderProps(props.className, (className) =>
-          cn(
-            "grid max-h-[calc(var(--visual-viewport-height)-10rem)] grid-cols-[auto_1fr] overflow-auto rounded-xl p-1 outline-hidden [clip-path:inset(0_0_0_0_round_calc(var(--radius-lg)-2px))] sm:max-h-[inherit] *:[[role='group']+[role=group]]:mt-4 *:[[role='group']+[role=separator]]:mt-1",
-            className
-          )
-        )}
-      />
-    </PopoverContent>
-  )
-}
+      {...props}
+    />
+  </PopoverContent>
+)
 
 type MenuItemProps = MenuItemPrimitiveProps &
   VariantProps<typeof dropdownItemStyles> & {
@@ -147,18 +121,18 @@ const MenuItem = ({ isDanger = false, children, ...props }: MenuItemProps) => {
 
   return (
     <MenuItemPrimitive
-      textValue={textValue}
       data-danger={isDanger ? "true" : undefined}
       {...props}
+      textValue={textValue}
       className={composeRenderProps(props.className, (className, renderProps) =>
         dropdownItemStyles({
           ...renderProps,
           className: renderProps.hasSubmenu
-            ? cn(
+            ? cn([
                 "data-open:data-danger:bg-danger/10 data-open:data-danger:text-danger",
                 "data-open:bg-accent data-open:text-accent-fg data-open:*:data-[slot=icon]:text-accent-fg data-open:*:[.text-muted-fg]:text-accent-fg",
                 className
-              )
+              ])
             : className
         })
       )}
@@ -190,38 +164,37 @@ type MenuHeaderProps = React.ComponentProps<typeof Header> & {
   separator?: boolean
 }
 
-const MenuHeader = ({
-  className,
-  separator = false,
-  ...props
-}: MenuHeaderProps) => (
+const MenuHeader = ({ separator = false, ...props }: MenuHeaderProps) => (
   <Header
+    {...props}
     className={cn(
       "col-span-full px-2.5 py-2 text-base font-semibold sm:text-sm",
       separator && "-mx-1 mb-1 border-b sm:px-3 sm:pb-[0.625rem]",
-      className
+      props.className
     )}
-    {...props}
   />
 )
 
 const { section, header } = dropdownSectionStyles()
 
 type MenuSectionProps<T> = MenuSectionPrimitiveProps<T> & {
-  ref?: React.Ref<HTMLDivElement>
   title?: string
 }
 
 const MenuSection = <T extends object>({
-  className,
-  ref,
+  title,
   ...props
-}: MenuSectionProps<T>) => (
-  <MenuSectionPrimitive ref={ref} className={section({ className })} {...props}>
-    {"title" in props && <Header className={header()}>{props.title}</Header>}
-    <Collection items={props.items}>{props.children}</Collection>
-  </MenuSectionPrimitive>
-)
+}: MenuSectionProps<T>) => {
+  return (
+    <MenuSectionPrimitive
+      {...props}
+      className={section({ className: props.className })}
+    >
+      {title && <Header className={header()}>{title}</Header>}
+      <Collection items={props.items}>{props.children}</Collection>
+    </MenuSectionPrimitive>
+  )
+}
 
 const MenuSeparator = DropdownSeparator
 const MenuItemDetails = DropdownItemDetails
