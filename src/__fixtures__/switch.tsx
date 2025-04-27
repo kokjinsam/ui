9962205d808c06ad7e16905e4b307af88ea6f8fb
Kok-Jin Sam <9153498+kokjinsam@users.noreply.github.com>
2025-04-27 14:30:55 +0800

    Update

---
+++ src/__fixtures__/switch.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import * as React from "react"
+import { Center } from "../center"
+import { Switch } from "../switch"
+
+export default () => {
+  return (
+    <Center>
+      <div className="grid gap-2">
+        <Switch>Switch Theme</Switch>
+        <Switch isDisabled>Switch Theme</Switch>
+      </div>
+    </Center>
+  )
+}
---
+++ src/switch.tsx
@@ -0,0 +1,50 @@
+"use client"
+
+import * as React from "react"
+import type { SwitchProps as SwitchPrimitiveProps } from "react-aria-components"
+import { Switch as SwitchPrimitive } from "react-aria-components"
+import { cn } from "./utils"
+
+type SwitchProps = SwitchPrimitiveProps
+
+const Switch = (props: SwitchProps) => (
+  <SwitchPrimitive
+    {...props}
+    className={cn(
+      "group text-ui-base inline-flex touch-none items-center",
+      props.className
+    )}
+    style={{ WebkitTapHighlightColor: "transparent" }}
+  >
+    {(values) => (
+      <>
+        <span
+          className={cn([
+            "bg-ui-line mr-2 h-5 w-8 cursor-pointer rounded-full border-2 border-transparent transition duration-200",
+            "group-data-[hovered]:bg-ui-line-hover",
+            "group-data-[focused]:ring-interactive-focus group-data-[focused]:ring-2",
+            "group-data-[disabled]:cursor-default group-data-[disabled]:opacity-50",
+            "group-data-[selected]:bg-interactive-accent"
+          ])}
+        >
+          <span
+            className={cn([
+              "bg-interactive-normal block size-4 origin-right rounded-full shadow-sm transition-all duration-200",
+              "forced-colors:disabled:outline-[GrayText]",
+
+              "group-data-[selected]:ml-3",
+              "group-data-[selected]:group-data-[pressed]:ml-2",
+              "group-data-[pressed]:w-5"
+            ])}
+          />
+        </span>
+        {typeof props.children === "function"
+          ? props.children(values)
+          : props.children}
+      </>
+    )}
+  </SwitchPrimitive>
+)
+
+export { Switch }
+export type { SwitchProps }
