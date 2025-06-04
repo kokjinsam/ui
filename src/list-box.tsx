e9515540a29bb37867be01e4fa0b9cabc179f6fb
Kok-Jin Sam <9153498+kokjinsam@users.noreply.github.com>
2025-06-04 17:26:41 +0800

    Update list box

--- src/list-box.tsx
+++ src/list-box.tsx
@@ -1,43 +1,31 @@
-"use client"
-
 import * as React from "react"
-import type {
-  ListBoxItemProps as ListBoxItemPrimitiveProps,
-  ListBoxProps as ListBoxPrimitiveProps
-} from "react-aria-components"
+import type { ListBoxItemProps, ListBoxProps } from "react-aria-components"
 import {
   ListBoxItem as ListBoxItemPrimitive,
   ListBox as ListBoxPrimitive,
   composeRenderProps
 } from "react-aria-components"
 import {
-  DropdownItemDetails,
+  DropdownDescription,
   DropdownLabel,
   DropdownSection,
   dropdownItemStyles
 } from "./dropdown"
-import { cn } from "./utils"
-
-type ListBoxProps<T extends object> = ListBoxPrimitiveProps<T>
+import { cn, composeClassName } from "./utils"
 
 const ListBox = <T extends object>(props: ListBoxProps<T>) => (
   <ListBoxPrimitive
     {...props}
-    className={cn(
-      "grid max-h-[inherit] w-full min-w-56 grid-cols-[auto_1fr] overflow-auto rounded-lg border px-1 pt-1 pb-1.5 shadow-lg outline-hidden",
-      "*:[[role='group']+[role=group]]:mt-4",
-      "*:[[role='group']+[role=separator]]:mt-1",
-      props.className
+    className={composeClassName(
+      props.className,
+      "grid max-h-96 w-full min-w-56 scroll-py-1 grid-cols-[auto_1fr] flex-col gap-y-1 overflow-y-scroll overscroll-contain rounded-xl border p-1 shadow-lg outline-hidden [scrollbar-width:thin] [&::-webkit-scrollbar]:size-0.5 *:[[role='group']+[role=group]]:mt-4 *:[[role='group']+[role=separator]]:mt-1"
     )}
   />
 )
 
-type ListBoxItemProps<T extends object> = ListBoxItemPrimitiveProps<T>
-
 const ListBoxItem = <T extends object>(props: ListBoxItemProps<T>) => {
   const textValue =
-    props.textValue ||
-    (typeof props.children === "string" ? props.children : undefined)
+    typeof props.children === "string" ? props.children : undefined
 
   return (
     <ListBoxItemPrimitive
@@ -58,18 +46,19 @@ const ListBoxItem = <T extends object>(props: ListBoxItemProps<T>) => {
           <>
             {allowsDragging && (
               <span
+                data-slot="icon"
                 className={cn(
-                  "text-muted size-4 shrink-0 transition",
-                  isFocused && "text-normal",
-                  isDragging && "text-normal",
-                  isSelected && "text-accent-fg/70"
+                  "lucide-grip-vertical text-muted-foreground size-4 shrink-0 transition",
+                  isFocused && "text-foreground",
+                  isDragging && "text-foreground",
+                  isSelected && "text-accent-foreground/70"
                 )}
               />
             )}
             {isSelected && (
               <span
                 data-slot="checked-icon"
-                className="lucide-check -mx-0.5 mr-2 size-4"
+                className="lucide-check -mx-0.5 mr-2"
               />
             )}
             {typeof props.children === "function" ? (
@@ -95,11 +84,19 @@ const ListBoxSection = (props: ListBoxSectionProps) => (
   />
 )
 
-const ListBoxItemDetails = DropdownItemDetails
+const ListBoxLabel = DropdownLabel
+const ListBoxDescription = DropdownDescription
 
 ListBox.Section = ListBoxSection
-ListBox.ItemDetails = ListBoxItemDetails
+ListBox.Label = ListBoxLabel
+ListBox.Description = ListBoxDescription
 ListBox.Item = ListBoxItem
 
-export { ListBox, ListBoxItem, ListBoxSection }
+export {
+  ListBox,
+  ListBoxDescription,
+  ListBoxItem,
+  ListBoxLabel,
+  ListBoxSection
+}
 export type { ListBoxItemProps, ListBoxSectionProps }
