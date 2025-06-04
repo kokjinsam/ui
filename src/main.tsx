1fc6aae7cc9f4ced85aa05562f178e21280cc88e
Kok-Jin Sam <9153498+kokjinsam@users.noreply.github.com>
2025-06-04 13:57:14 +0800

    Remove stuffs

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react"
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <h1>Main</h1>
-  </StrictMode>
-)
